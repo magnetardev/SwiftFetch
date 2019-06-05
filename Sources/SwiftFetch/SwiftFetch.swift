75769f5a384f959fe6f32bc24e12ad2c8a1e45f9
Magnetar <magnetardev@icloud.com>
2019-06-05 17:13:54 -0400

    Ok Epic

--- Sources/SwiftFetch/SwiftFetch.swift
+++ Sources/SwiftFetch/SwiftFetch.swift
@@ -1,49 +1,46 @@
-import Swift
 import Foundation
 import Bluebird
 
-public struct SwiftFetch {
-    public class FetchResponse {
-        // Init Variables
-        let data: Data;
-        let response: URLResponse;
-        
-        init(data: Data, response: URLResponse) {
-            self.response = response;
-            self.data = data;
-        }
-        
-        func text() -> String {
-            return String(data: self.data, encoding: String.Encoding.utf8)!
-        }
-        
-        func json<T: Decodable>(handler: T.Type) -> Any {
-            let decoder = JSONDecoder();
-            do {
-                let data = try decoder.decode(handler, from: self.data)
-                return data;
-            } catch let err {
-                return err
-            }
-        }
+public class FetchResponse {
+    // Init Variables
+    let data: Data;
+    let response: URLResponse;
+    
+    init(data: Data, response: URLResponse) {
+        self.response = response;
+        self.data = data;
     }
     
-    public func fetch(url: URL) -> Promise<FetchResponse> {
-        return Promise<FetchResponse> { resolve, reject in
-            DispatchQueue(label: "fetch.operation").async {
-                let request = NSMutableURLRequest(url: url);
-                let session = URLSession.shared;
-                request.httpMethod = "GET";
-                let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, error -> Void in
-                    if (error != nil) {
-                        reject(error!);
-                    } else {
-                        let responseData = FetchResponse(data: data!, response: response!)
-                        resolve(responseData);
-                    }
-                });
-                task.resume();
-            }
+    func text() -> String {
+        return String(data: self.data, encoding: String.Encoding.utf8)!
+    }
+    
+    func json<T: Decodable>(handler: T.Type) -> Any {
+        let decoder = JSONDecoder();
+        do {
+            let data = try decoder.decode(handler, from: self.data)
+            return data;
+        } catch let err {
+            return err
+        }
+    }
+}
+
+public func fetch(url: URL) -> Promise<FetchResponse> {
+    return Promise<FetchResponse> { resolve, reject in
+        DispatchQueue(label: "fetch.operation").async {
+            let request = NSMutableURLRequest(url: url);
+            let session = URLSession.shared;
+            request.httpMethod = "GET";
+            let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, error -> Void in
+                if (error != nil) {
+                    reject(error!);
+                } else {
+                    let responseData = FetchResponse(data: data!, response: response!)
+                    resolve(responseData);
+                }
+            });
+            task.resume();
         }
     }
 }
